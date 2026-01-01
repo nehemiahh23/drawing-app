b1c5497061eb47941ebe5430b0a43550684924b5
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-01-01 12:02:25 -0500

    structure initial card template

--- scripts/browse.js
+++ scripts/browse.js
@@ -1,1 +1,11 @@
 const main = document.querySelector("main")
+
+const imgCard = document.createDocumentFragment()
+const cardContainer = document.createElement("div")
+const img = document.createElement("img")
+
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png"
+
+cardContainer.appendChild(img)
+imgCard.appendChild(cardContainer)
+main.appendChild(imgCard)
