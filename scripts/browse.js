2695e114f1a8e2c9e01ee04b80bf39d9dff1c5dd
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-01-01 12:52:58 -0500

    render templated content from db

--- scripts/browse.js
+++ scripts/browse.js
@@ -1,11 +1,22 @@
 const main = document.querySelector("main")
+const container = document.createDocumentFragment()
 
-const imgCard = document.createDocumentFragment()
-const cardContainer = document.createElement("div")
-const img = document.createElement("img")
+fetch("http://localhost:3002/images")
+.then(r => r.json())
+.then(data => {
+	data.forEach(img => container.appendChild(renderCard(img)))
+	main.appendChild(container)
+})
 
-img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aspect-ratio-16x9.svg/1200px-Aspect-ratio-16x9.svg.png"
+function renderCard(data) {
+	const card = document.createElement("div")
+	const img = document.createElement("img")
+	const title = document.createElement("h3")
 
-cardContainer.appendChild(img)
-imgCard.appendChild(cardContainer)
-main.appendChild(imgCard)
+	img.src = data.src
+	title.textContent = data.title
+	card.appendChild(img)
+	card.appendChild(title)
+
+	return card
+}
