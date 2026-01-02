ce67a2f56f8cd99aad3e24d54477d29ed76db549
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-01-01 20:52:04 -0500

    add basic hover functionality

--- scripts/browse.js
+++ scripts/browse.js
@@ -8,17 +8,25 @@ fetch("http://localhost:3002/images")
 	main.appendChild(container)
 })
 
+main.addEventListener("mouseover", handleHover)
+main.addEventListener("mouseout", handleHover)
+
 function renderCard(data) {
 	const card = document.createElement("div")
 	const img = document.createElement("img")
 	const title = document.createElement("h3")
 
 	card.className = "card"
-	card.id = `img${data.id}`
+	// card.id = `img${data.id}`
 	img.src = data.src
 	title.textContent = data.title
 	card.appendChild(img)
 	card.appendChild(title)
-
+	
 	return card
+}
+
+function handleHover(e) {
+	if (e.target.tagName !== "IMG") return
+	e.type === "mouseover" ? e.target.style.height = "110%" : e.target.style.height = "100%"	
 }
