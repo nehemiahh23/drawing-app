a3774200703e65523b6123460eb2ccf6b0bf6edb
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-03-10 17:40:51 -0400

    store 404 img for placeholder

--- server/controllers/drawingController.ts
+++ server/controllers/drawingController.ts
@@ -38,7 +38,7 @@ export async function createDrawing(rq: AuthRequest, rs: Response) {
 	try {
 		newDrawing = await Drawing.create({
 			...rq.body,
-			src: "https://res.cloudinary.com/ddka2pw9a/image/upload/v1772767575/2153483_e8wtpj.jpg",
+			src: "https://res.cloudinary.com/ddka2pw9a/image/upload/v1773178805/69b08faa513358e4412d8505.png",
 			userId: payload.user.id,
 			locked: false
 		})
