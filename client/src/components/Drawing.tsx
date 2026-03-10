59042716fc69a1b99987b1d03f1ca4f935783c7e
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-03-10 12:35:55 -0400

    setup deletion marking

--- client/src/components/Drawing.tsx
+++ client/src/components/Drawing.tsx
@@ -1,8 +1,36 @@
-function Drawing({ src, title }) {
-  return (
+import { use, useState } from "react"
+import axios from "axios"
+
+function Drawing({ id, src, title, locked, markedList, setMarkedList }) {
+	const [marked, setMarked] = useState(false)
+	
+	function handlePost() {
+		
+	}
+	
+	function handleEdit() {
+
+	}
+	
+	function handleDelete(e) {
+		// axios.delete(`http://localhost:3000/api/drawings/${id}`)
+		// .then()
+		if (!marked) {
+			setMarkedList(list => [...list, id])
+			setMarked(true)
+		} else {
+			setMarkedList(list => list.filter(i => i !== id))
+			setMarked(false)
+		}
+	}
+	
+	return (
 	<div>
 		<p>{title}</p>
 		<img src={src} alt={title} />
+		<button>{ locked ? "View Post" : "Create Post" }</button>
+		<button>Edit</button>
+		<button onClick={handleDelete}>{ marked ? "Marked For Deletion" : "Delete" }</button>
 	</div>
   )
 }
--- client/src/pages/Portfolio/Portfolio.tsx
+++ client/src/pages/Portfolio/Portfolio.tsx
@@ -4,8 +4,9 @@ import axios from "axios"
 
 function Portfolio({ drawings, setDrawings }) {
 	const [refresh, setRefresh] = useState(false)
+	const [markedList, setMarkedList] = useState<string[]>([])
 
-	useEffect(() => {
+	useEffect(() => { // TODO: Add a delete multiple route to optimize this behavior
 		if (!drawings.length || refresh ) {
 			axios.get("http://localhost:3000/users/user/drawings")
 			.then(r => setDrawings(r.data))
@@ -21,7 +22,7 @@ function Portfolio({ drawings, setDrawings }) {
 	return (
 	<div>
 		<button onClick={handleRefresh}>Refesh</button>
-		{ drawings.map(d => <Drawing key={d._id} src={d.src} title={d.title} />) }
+		{ drawings.map(d => <Drawing key={d._id} id={d._id} src={d.src} title={d.title} locked={d.locked} markedList={markedList} setMarkedList={setMarkedList} />) }
 	</div>
   )
 }
